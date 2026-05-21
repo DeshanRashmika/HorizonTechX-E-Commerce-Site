038cf47880a4afc8938497bf93eaa68d596d6063
Deshan Rashmika <deshanrashmika002@gmail.com>
2026-05-21 12:04:59 +0530

    feat: restructure server setup to include user and order routes with CORS support

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config(); 
+const cors = require('cors');
+require('dotenv').config();
+
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Database connected successfully!'))
     .catch((err) => console.error('Database connection error:', err));
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
     res.send('E-Commerce Backend Server with MongoDB is running!');
@@ -16,8 +27,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
-
-const productRoutes = require('./routes/productRoutes');
-
-app.use('/api/products', productRoutes);
+});
